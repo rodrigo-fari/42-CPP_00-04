ce51b7b40ade01a9e403894764a55063e5399745
Rodrigo Falcao <rodrigofal44@gmail.com>
2025-11-10 21:36:58 +0000

    Begin - CPP04EX00

---
+++ cppModule-04/ex00/Animal.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/10 20:47:20 by rde-fari          #+#    #+#             */
+/*   Updated: 2025/11/10 21:36:21 by rde-fari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <string>
+# include <cctype>
+# include <iostream>
+# include <sstream>
+
+class Animal {
+	protected:
+		std::string	type;
+	public:
+		Animal();
+		void	setType(std::string type);
+		void	getType(std::string type);
+};
+
+#endif
